ea5355a6582978af200e908956fe15e62dbabf31
Sergio-GomezA <s2441782@ed.ac.uk>
2022-11-16 11:29:24 +0000

    adding extra code for Hessian

---
+++ hess_temp.R
@@ -0,0 +1,13 @@
+
+
+approx.Hess <- function(theta,grad){
+  #'
+  #'Calculates a numerical approximation of Hessian at value "theta"
+  #'Using the gradient "grad"
+  #'
+  #'Inputs:
+  #'Outputs:
+  #'
+  #'
+  #'
+}
